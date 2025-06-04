2c805be59d799b3ea852a64c4b2d7a7051c02ec4
Tolulope <malomo.tolz@gmail.com>
2025-06-04 23:53:23 +0100

    fix port

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,6 @@ import app from './app';
 import env from './config/env';
 import logger from './utils/logger';
 
-app.listen(env.PORT ?? process.env.PORT, () => {
+app.listen(process.env.PORT || env.PORT, () => {
   logger.info('Server started at http://localhost:%s', env.PORT);
 });
